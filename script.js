1c84b8fa1102c12372f69664adc8aef3e415fb97
sohan2410 <bandarysohan24@gmail.com>
2021-05-18 12:31:57 +0530

    added table

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 let userArray = [];
-const container = document.querySelector(".container");
+const container = document.querySelector(".card");
 const today = new Date();
 const date =
-  today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
 let pincode;
 const pincodeButton = document.querySelector(".pincodeButton");
+
+
+const createCardList = (array) => {
+//   container.innerHTML = "";
+
+  array = array.centers;
+  array.forEach((obj) => {
+    const card = document.createElement("tr");
+    card.classList.add("card");
+
+    card.innerHTML=`<tr>
+    <th>${obj.center_id}</th>
+    <th>${obj.name}</th>
+    <th>${obj.address}</th>
+    <th>${obj.fee_type}</th>
+</tr>`;
+
+    obj=obj.sessions;
+
+    obj.forEach((item) => {
+        card.innerHTML+=`
+        <table>
+        <tr>
+            <td>${item.available_capacity}</td>
+            <td>${item.available_capacity_dose1}</td>
+            <td>${item.available_capacity_dose2}</td>
+            <td>${item.date}</td>
+            <td>${item.min_age_limit}</td>
+            <td>${item.vaccine}</td>
+        </tr>
+        </table>
+        `
+    })
+    container.appendChild(card);
+  });
+};
 pincodeButton.addEventListener("click", () => {
-  pincode = prompt("Enter PinCode to search for covid vaccines in India");
-  const apiUrl =
+    pincode = prompt("Enter PinCode to search for covid vaccines in India");
+    const apiUrl =
     "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=" +
     pincode +
     "&date=" +
@@ -24,50 +60,3 @@ pincodeButton.addEventListener("click", () => {
     });
 });
 
-const createCardList = (array) => {
-  container.innerHTML = "";
-
-  array = array.centers;
-  array.forEach((obj) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `<div class="center-id">
-    <span>Center Id: </span>
-    <span>${obj.center_id}</span>
-  </div>
-
-  <div class="name">
-    <span>Name: </span>
-    <span>${obj.name}</span>
-  </div>
-
-  <div class="address">
-    <span>Address: </span>
-    <span>${obj.address}</span>
-  </div>
-
-  <div class="district_name">
-    <span>District Name: </span>
-    <span>${obj.district_name}</span>
-  </div>
-
-  <div class="pincode">
-          <span>Pincode:  </span>
-          <span>${obj.pincode}</span>
-        </div>
-
-  <div class="state_name">
-    <span>State Name: </span>
-    <span>${obj.state_name}</span>
-  </div>
-
-  <div class="fee_type">
-    <span>Fee Type: </span>
-    <span>${obj.fee_type}</span>
-  </div>`;
-
-    container.appendChild(card);
-  });
-};
-
-console.log("before fetch");
